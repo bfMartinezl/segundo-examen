c6789a3afee7c5224097a9a54a5f36eb0e8a94a0
bfmartinezl <bm174917@gmail.com>
2022-08-20 21:50:22 -0600

    creacion de archivo

---
+++ Examen dos.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 20 20:41:25 2022
+
+@author: HP EliteBook 2470p
+"""
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import os
+import snappy
+from snappy import Product
+from snappy import ProductIO
+from snappy import ProductUtils
+from snappy import WKTReader
+from snappy import HashMap
+from snappy import GPF
+# Para leer shapefiles
+import shapefile
+import pygeoif
